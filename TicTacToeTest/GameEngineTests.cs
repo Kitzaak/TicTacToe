1e0e67953cf016935a869ff1932d7500583b1c5c
tunruh <travis.unruh@samaritanministries.org>
2022-10-22 00:35:36 -0500

    Converted end of game tests to one theroy

--- TicTacToeTest/GameEngineTests.cs
+++ TicTacToeTest/GameEngineTests.cs
@@ -69,95 +69,7 @@ public class UnitTest1
 
         Assert.False(engine.IsOver());
     }
-
-    [Fact]
-    public void game_over_when_all_fields_filled()
-    {
-        var engine = new Engine();
-
-        var pieces = new List<char>
-        {
-            'X', 'X', 'O',
-            'O', 'O', 'X',
-            'X', 'O', 'X'
-        };
-        engine.Move(1, 1);
-        engine.Move(2, 2);        
-        engine.Move(1, 2);
-        engine.Move(1, 3);
-        engine.Move(3, 1);
-        engine.Move(2, 1);
-        engine.Move(2, 3);
-        engine.Move(3, 2);
-        engine.Move(3, 3);
-
-        Assert.Equal(pieces, engine.Pieces());
-        Assert.True(engine.IsOver());
-    }
-
-    [Fact]
-    public void game_over_when_same_in_row()
-    {
-        var engine = new Engine();
-
-        var pieces = new List<char>
-        {
-            ' ', ' ', ' ',
-            'O', 'O', ' ',
-            'X', 'X', 'X'
-        };
-        engine.Move(3, 1);
-        engine.Move(2, 2);        
-        engine.Move(3, 2);
-        engine.Move(2, 1);
-        engine.Move(3, 3);
-
-        Assert.Equal(pieces, engine.Pieces());
-        Assert.True(engine.IsOver());
-    }
-
-    [Fact]
-    public void game_over_when_same_in_col()
-    {
-        var engine = new Engine();
-
-        var pieces = new List<char>
-        {
-            ' ', ' ', 'X',
-            'O', 'O', 'X',
-            ' ', ' ', 'X'
-        };
-        engine.Move(2, 3);
-        engine.Move(2, 2);        
-        engine.Move(1, 3);
-        engine.Move(2, 1);
-        engine.Move(3, 3);
-
-        Assert.Equal(pieces, engine.Pieces());
-        Assert.True(engine.IsOver());
-    }
-
-    [Fact]
-    public void game_over_when_same_in_diagonal()
-    {
-        var engine = new Engine();
-
-        var pieces = new List<char>
-        {
-            'X', ' ', ' ',
-            'O', 'X', 'O',
-            ' ', ' ', 'X'
-        };
-        engine.Move(2, 2);
-        engine.Move(2, 1);        
-        engine.Move(1, 1);
-        engine.Move(2, 3);
-        engine.Move(3, 3);
-
-        Assert.Equal(pieces, engine.Pieces());
-        Assert.True(engine.IsOver());
-    }
-
+    
     [Theory]
     [InlineData('X', ' ', ' ', 'X', 'O', 'O', ' ', ' ', 'X', 'O')]
     [InlineData('X', ' ', ' ', 'X', 'O', 'O', 'O', ' ', 'X', 'X')]
@@ -174,5 +86,29 @@ public class UnitTest1
 
         Assert.Equal(pieces, engine.Pieces());
         Assert.Equal(NextMark, engine.NextMark);
+    }
+
+    [Theory]
+    [InlineData('X', 'X', 'O', 'O', 'X', 'X', 'X', 'O', 'O')]
+    [InlineData('X', 'X', 'X', ' ', 'O', ' ', ' ', 'O', ' ')]
+    [InlineData('X', 'X', ' ', 'O', 'O', 'O', ' ', 'X', ' ')]
+    [InlineData(' ', ' ', 'O', ' ', 'O', ' ', 'X', 'X', 'X')]
+    [InlineData('X', ' ', ' ', 'X', 'O', 'O', 'X', ' ', ' ')]
+    [InlineData('X', 'O', 'O', ' ', 'O', ' ', 'X', 'O', ' ')]
+    [InlineData('O', 'O', 'X', ' ', ' ', 'X', ' ', ' ', 'X')]
+    [InlineData('X', 'O', ' ', ' ', 'X', ' ', ' ', 'O', 'X')]
+    [InlineData('X', 'X', 'O', ' ', 'O', ' ', 'O', 'X', ' ')]
+    public void game_over(char field1, char field2, char field3, char field4, char field5, char field6, char field7, char field8, char field9)
+    {
+        var pieces = new List<char>
+        {
+            field1, field2, field3,
+            field4, field5, field6,
+            field7, field8, field9
+        };
+
+        var engine = new Engine(pieces);
+
+        Assert.True(engine.IsOver());
     }
 }
